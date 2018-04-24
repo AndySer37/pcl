dbb5b1b95ffc600d461000eeb210594d5bd6074c
championway <cpwearth.eed03@g2.nctu.edu.tw>
2018-04-24 16:46:03 +0800

    clustering with ros publish and different color presentation

--- catkin_ws/src/planar_segmentation/src/cluster_ros.cpp
+++ catkin_ws/src/planar_segmentation/src/cluster_ros.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <cmath>        // std::abs
 #include <sensor_msgs/PointCloud2.h>
 #include "pcl_ros/point_cloud.h"
 #include <pcl/io/io.h>
@@ -16,16 +17,54 @@
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/segmentation/extract_clusters.h>
 
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloudXYZ;
 typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudXYZRGB;
+PointCloudXYZ::Ptr cloud_inXYZ (new PointCloudXYZ);
 PointCloudXYZRGB::Ptr cloud_in (new PointCloudXYZRGB); 
 PointCloudXYZRGB::Ptr cloud_filtered (new PointCloudXYZRGB);
 PointCloudXYZRGB::Ptr cloud_f (new PointCloudXYZRGB);
 PointCloudXYZRGB::Ptr cloud_plane (new PointCloudXYZRGB);
+ros::Publisher pub_XYZRGB;
+bool lock = false;
+void cluster_pointcloud(void);
 
 void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
 {
-  sensor_msgs::PointCloud2 pcl_to_ros_pointcloud2;
-  pcl::fromROSMsg (*input, *cloud_in);
+  if (!lock){
+    lock = true;
+    // covert from ros type to pcl type
+    pcl::fromROSMsg (*input, *cloud_inXYZ);
+    copyPointCloud(*cloud_inXYZ, *cloud_in);
+    for (size_t i = 0; i < cloud_in->points.size(); i++){
+      cloud_in->points[i].r = 0;
+      cloud_in->points[i].g = 0;
+      cloud_in->points[i].b = 0;
+    }
+    cluster_pointcloud();
+  }
+  else{
+    std::cout << "lock" << std::endl;
+  }
+}
+
+int point_cloud_color(int input){
+  if(input < 0){
+    return 0;
+  }
+  else if(input > 255){
+    return 255;
+    std::cout << "WTF" << std::endl;
+  }
+  else{
+    return input;
+  }
+}
+
+//void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
+void cluster_pointcloud()
+{
+  //sensor_msgs::PointCloud2 pcl_to_ros_pointcloud2;
+  //pcl::fromROSMsg (*input, *cloud_in);
   // Remove NaN point
   std::vector<int> indices;
   pcl::removeNaNFromPointCloud(*cloud_in, *cloud_in, indices);
@@ -59,7 +98,7 @@ void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
     if (inliers->indices.size () == 0)
     {
       std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
-      break;
+      //break;
     }
 
     // Extract the planar inliers from the input cloud
@@ -70,7 +109,7 @@ void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
 
     // Get the points associated with the planar surface
     extract.filter (*cloud_plane);
-    std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size () << " data points." << std::endl;
+    //std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size () << " data points." << std::endl;
 
     // Remove the planar inliers, extract the rest
     extract.setNegative (true);
@@ -84,36 +123,65 @@ void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
 
   std::vector<pcl::PointIndices> cluster_indices;
   pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
-  ec.setClusterTolerance (0.02); // 2cm
+  ec.setClusterTolerance (0.2); // 2cm
   ec.setMinClusterSize (100);
-  ec.setMaxClusterSize (25000);
+  ec.setMaxClusterSize (55000);
   ec.setSearchMethod (tree);
   ec.setInputCloud (cloud_filtered);
   ec.extract (cluster_indices);
 
   int j = 0;
   int a = 0;
+  int set_r=0, set_g=0, set_b=0;
   for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
   {
+    //set_r -= a;
+    //set_g += a;
+    //set_b += a;
+    
+    Eigen::Vector4f centroid;
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZRGB>);
     for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
+    {
       cloud_cluster->points.push_back (cloud_filtered->points[*pit]); //*
-    cloud_cluster->width = cloud_cluster->points.size ();
-    cloud_cluster->height = 1;
-    cloud_cluster->is_dense = true;
-
-    std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
-    std::stringstream ss;
-    ss << "cloud_cluster_" << j << ".pcd";
-    
-    for (size_t i = 0; i < cloud_cluster->points.size(); i++){
-      cloud_cluster->points[i].r = 255 - a;
-      cloud_cluster->points[i].g = 0 + a;
+      /*cloud_filtered->points[*pit].r = set_r;
+      cloud_filtered->points[*pit].g = set_g;
+      cloud_filtered->points[*pit].b = set_b;*/
     }
-    writer.write<pcl::PointXYZRGB> (ss.str (), *cloud_cluster, false); //*
+    pcl::compute3DCentroid(*cloud_cluster, centroid);
+    //std::cout << centroid << std::endl;
+
+    set_r = point_cloud_color(int(255 - std::abs(30*centroid[1])));
+    set_g = point_cloud_color(int(std::abs(30*centroid[1])));
+    set_b = 0;
+    for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
+    {
+      cloud_filtered->points[*pit].r = set_r;
+      cloud_filtered->points[*pit].g = set_g;
+      cloud_filtered->points[*pit].b = set_b;
+    }
+    //cloud_cluster->width = cloud_cluster->points.size ();
+    //cloud_cluster->height = 1;
+    //cloud_cluster->is_dense = true;
+
+    //std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
+    //std::stringstream ss;
+    //ss << "cloud_cluster_" << j << ".pcd";
+    
+    /*for (size_t i = 0; i < cloud_filtered->points.size(); i++){
+      cloud_filtered->points[i].r = 255;
+      cloud_filtered->points[i].g = 255;
+      cloud_filtered->points[i].b = 0;
+    }*/
+    //std::cout << "cluster" << std::endl;
     a+=30;
     j++;
   }
+
+  //writer.write<pcl::PointXYZRGB> ("result.pcd", *cloud_filtered, false);
+  std::cout << j << std::endl << "Finish" << std::endl << std::endl;
+  pub_XYZRGB.publish(*cloud_filtered);
+  lock = false;
 }
 
 int main (int argc, char** argv)
@@ -123,11 +191,11 @@ int main (int argc, char** argv)
      ros::NodeHandle nh;
      //cluster();
      // Create a ROS subscriber for the input point cloud
-     ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("/cloud_pcd", 1, cloud_cb);
+     ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("/velodyne_points", 1, cloud_cb);
      // Create a ROS publisher for the output point cloud
      //pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1); 
-     //pointcloudXYZ = nh.advertise<PointCloudXYZ> ("ros_pointcloudxyz", 1);
+     pub_XYZRGB = nh.advertise<PointCloudXYZRGB> ("ros_pointcloudxyz", 1);
      //pointcloud2_publisher = nh.advertise<sensor_msgs::PointCloud2> ("pcltoros_pointcloud2", 1);
      // Spin
      ros::spin ();
-  }
+}
